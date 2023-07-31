3c9930975ce937bd9341e12043da0a0b95669eca
Ashley Jisue Hong <ahpuppy63@gmail.com>
2023-07-31 10:03:04 -1000

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Ashley Hong on 2023/07/31.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
